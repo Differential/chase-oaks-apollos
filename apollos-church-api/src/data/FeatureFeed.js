dfb6122dc3c8fa08f5019bcdbfd067d4982f1c48
nlewis84 <nathan@differential.com>
2021-11-09 15:35:21 -0600

    removed unneeded custom resolver and added changes to correct index file

--- apollos-church-api/src/data/FeatureFeed.js
+++ apollos-church-api/src/data/FeatureFeed.js
@@ -1,19 +1,19 @@
 import { FeatureFeed } from '@apollosproject/data-connector-rock';
 import gql from 'graphql-tag';
 
-const { dataSource } = FeatureFeed;
+const { dataSource, resolver } = FeatureFeed;
 
-const resolver = {
-  ...FeatureFeed.resolver,
-  Query: {
-    ...FeatureFeed.resolver.Query,
-    tvFeedFeatures: (root, args, { dataSources }) =>
-      dataSources.FeatureFeed.getFeed({
-        type: 'apollosConfig',
-        args: { section: 'TV_FEATURES', ...args },
-      }),
-  },
-};
+// const resolver = {
+//   ...FeatureFeed.resolver,
+//   Query: {
+//     ...FeatureFeed.resolver.Query,
+//     tvFeedFeatures: (root, args, { dataSources }) =>
+//       dataSources.FeatureFeed.getFeed({
+//         type: 'apollosConfig',
+//         args: { section: 'TV_FEATURES', ...args },
+//       }),
+//   },
+// };
 
 const schema = gql`
   extend enum Tab {
